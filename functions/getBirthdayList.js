f77624470434bc31097b280eee725a123254f30d
k3shavv <keshavrawat999.kr@gmail.com>
2022-02-18 23:03:10 +0530

    started impleting getting birthday List

---
+++ functions/getBirthdayList.js
@@ -0,0 +1,21 @@
+const data=require('../data/jsonData/details.json')
+
+
+
+module.exports=getTodaysList=()=>{
+
+    let listofBirthdays=[];
+    let today = new Date(); 
+
+    console.log(today);
+
+    // let y=getJsDateFromExcel(x);
+
+    // console.log(y);
+    // console.log(y.getDate());
+    // console.log(today);
+    // console.log(today.getDay());
+    // console.log(today.getMonth()+1);
+    return listofBirthdays;
+
+}
--- functions/readExcel.js
+++ functions/readExcel.js
@@ -1,25 +1,20 @@
 const reader=require('xlsx');
 const fs=require('fs');
 
-module.exports= readExcel=async (address)=>{
+
+
+const readExcel=async (address)=>{
     
     const file =await reader.readFile(address);
     const sheets=file.SheetNames;
     let data = [];
 
     for(let i = 0; i < sheets.length; i++)
-    {
-        const sheet={
-            NameOfSheet:correct_formatting(sheets[i]),
-            sheetData:[]
-        }
-
-        data.push(sheet);
+    {      
         const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]]);
         temp.forEach((res) => {
-            data[i].sheetData.push(res);
+            data.push(res);
         })
-
     }
     saveToFile(data);
 }
@@ -27,16 +22,13 @@ module.exports= readExcel=async (address)=>{
  
 const saveToFile= async (data)=>{
 
-    data.forEach(sheet=>{
-        const dataString=JSON.stringify(sheet.sheetData,null,2);
-        fs.writeFile('./data/jsonData/'+sheet.NameOfSheet+'.json',dataString,err=>{
-            if (err) {
-                console.log('Error writing file', err)
-            } 
-            console.log('Successfully wrote file')
-            
-        })   
-    })
+    const dataString=JSON.stringify(data,null,2);
+    fs.writeFile('./data/jsonData/details.json',dataString,err=>{
+        if (err) {
+            console.log('Error writing file', err)
+        } 
+        console.log('Successfully wrote file')
+    })   
 }
 
 
@@ -44,7 +36,6 @@ const saveToFile= async (data)=>{
  * corrects the formatting of sheetName 
  */
 const correct_formatting=(sheetName)=>{
-
     let comp=sheetName.split(' ');
     let correctName="";
     comp.forEach(item=>{
@@ -52,3 +43,12 @@ const correct_formatting=(sheetName)=>{
     })
     return correctName;
 }
+
+function getJsDateFromExcel(excelDate) {
+      return new Date((excelDate - (25567 + 2))*86400*1000);
+}
+
+
+
+
+module.exports=readExcel;
