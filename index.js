08091103172facb859641cfd1cb9bc2b90e5dfc1
k3shavv <keshavrawat999.kr@gmail.com>
2022-02-28 22:27:55 +0530

    added start script

--- index.js
+++ index.js
@@ -21,10 +21,12 @@ app.get('/',(req,res)=>{
 })
 
 //run the script //   0 10 * * *
-const job = schedule.scheduleJob("*/20 * * * * *", async function () {
+const job = schedule.scheduleJob("*/30 * * * * *", async function () {
   console.log("starting...");
   const list = getTodaysList();
   console.log(list);
+  if(list.length==0)
+    return;
 
   if (isDataValid(list[0])) {
     await sendEmail(
